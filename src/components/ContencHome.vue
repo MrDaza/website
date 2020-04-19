db5898bc3df666b58f0ce119508b1cafb63b0c65
MacBook MrDaza <mr.daza@ingenieros.com>
2020-04-19 16:02:12 -0500

    🐞 Eliminación de TypeScript se configura para JavaScript

---
+++ src/components/ContencHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>
+      The COVID-19 Colombia Project collects and publishes the complete epidemiologic indicators and case data available for Colombia departments, municipalities, and cities. Besides, it is a space to show ideas or applications that help cope with the pandemic.
+    </p>
+    <p>
+      This website is a resource to help advance the understanding of the virus and inform the public, in order to save lives by making better decisions through monitoring the outbreak.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
